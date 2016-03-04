d17fd6a34d93ba9a88b3aacabec90f7d420c055a
Scott Bishop <treewojima@gmail.com>
2016-03-04 18:31:33 -0500

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
+        ts: {
+            build: {
+                options: {
+                    target: "es6",
+                    fast: "never",
+                    module: "commonjs"
+                },
+                src: ["src/**/*.ts", "!node_modules/**/*.ts"],
+                outDir: "js"
+            }
+        },
+
+        tslint: {
+            options: {
+                configuration: grunt.file.readJSON("tslint.json")
+            },
+            all: {
+                src: ["src/**/*.ts", "!node_modules/**/*.ts", "!obj/**/*.ts", "!typings/**/*.ts"]
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ["src/**/*.ts", "!node_modules/**/*.ts"],
+                tasks: ["newer:tslint:all", "ts:build"],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+
+        nodemon: {
+            dev: {
+                script: "js/server.js"
+            },
+            options: {
+                ignore: ["node_modules/**", "gruntfile.js"],
+                env: {
+                    PORT: "8080"
+                }
+            }
+        },
+
+        concurrent: {
+            watchers: {
+                tasks: ['nodemon', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks("grunt-tslint");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-newer");
+    grunt.loadNpmTasks("grunt-nodemon");
+    grunt.loadNpmTasks("grunt-concurrent");
+
+    grunt.registerTask("default", ["tslint:all", "ts:build"]);
+    grunt.registerTask("serve", ["concurrent:watchers"]);
+}
+
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+///<reference path="../typings/browser.d.ts" />
+
+import * as http from "http";
+
+http.createServer(function(req, res) {
+    console.log("Connection from " + req.socket.remoteAddress);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello, world!");
+    res.end();
+}).listen(8080);
+console.log("Server running on 8080");
+
